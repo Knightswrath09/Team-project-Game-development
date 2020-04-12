644254c69e98d37cf31e568ab2e27e7d6a2e0098
ssondericker <63079931+ssondericker@users.noreply.github.com>
2020-04-12 17:28:24 -0600

    Revert "Add files via upload"
    This reverts commit dbb27cfc30a547fd3901f536a8d9f1f0c77370a7.

--- Shield.cs
+++ Shield.cs
@@ -12,7 +12,6 @@ namespace TeamProject
     {
         private Texture2D texture;
         private Vector2 position;
-        //red shield 0, blue 1, purple 2
         private int indexInList;
         public Vector2 Position
         {
@@ -20,31 +19,17 @@ namespace TeamProject
             set { position = value; }
         }
 
-        //0 = top, 1 = right, 2 = bottom, 3 = left (can use direction enumerator as index)
-        //stores positions for each direction that the shield could be in
-        //saves time because we dont have to use if states and repeat math to determine position
-        private List<Vector2> PositionsByDirection;
-        
-        private Vector2 size;
-        
         //for purple shields
         //red and blue visible and purple insivible when red and blue are not overalapping
         //red and blue not visible and purple visible when red and blue are overlapping
         public bool visible { get; set; }
 
         //***SOPHIE
-        public Shield(CombatSpriteColors newColor, Directions newDirection, int newIndex, Texture2D newTexture, 
-            Vector2 newSize, ShipSprite theShip)
+        public Shield(CombatSpriteColors newColor, Directions newDirection, int newIndex)
         {
             SpriteColor = newColor;
             Direction = newDirection;
             indexInList = newIndex;
-            texture = newTexture;
-            size = newSize;
-
-            PositionsByDirection[0] = new Vector2((theShip.Position.X + (theShip.Size.X / 2)) - (size.X / 2),
-                theShip.Position.Y - (2 * size.Y));
-            PositionsByDirection[1] = new Vector2(theShip.Position.X + theShip.Size.X + (2 * size.Y), )
             
             //texture and position will depend on color and direction respectively
         }
